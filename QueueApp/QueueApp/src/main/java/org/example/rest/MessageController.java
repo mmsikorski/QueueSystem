61a4cd6643ff2598b6ffef154387b11ba09c92ee
mmsikorski <mm.sikorski3@gmail.com>
2023-01-15 17:57:19 +0100

    Set controller as a deprecated

--- QueueApp/QueueApp/src/main/java/org/example/rest/MessageController.java
+++ QueueApp/QueueApp/src/main/java/org/example/rest/MessageController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Deprecated
 @RequestMapping("/message")
 @RestController
 public class MessageController {
