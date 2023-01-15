adf417d76d8c43c56b0a44d10b71db3de6019876
mmsikorski <mm.sikorski3@gmail.com>
2023-01-15 17:58:20 +0100

    Add init scheduler code

---
+++ QueueApp/QueueApp/src/main/java/org/example/core/scheduler/job/creator/SchedulerJobCreator.java
@@ -0,0 +1,8 @@
+package org.example.core.scheduler.job.creator;
+
+import org.example.core.scheduler.job.model.JobModel;
+
+public interface SchedulerJobCreator {
+
+    public JobModel createJob();
+}
---
+++ QueueApp/QueueApp/src/main/java/org/example/core/scheduler/job/model/JobStorage.java
@@ -0,0 +1,6 @@
+package org.example.core.scheduler.job.model;
+
+public final class JobStorage {
+
+
+}
