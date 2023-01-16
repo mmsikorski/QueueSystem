527f730bcafacfbc96036eac69ece066ee98c247
mmsikorski <mm.sikorski3@gmail.com>
2023-01-16 19:07:02 +0100

    Add server creation service.

--- QueueApp/QueueApp/src/main/java/org/example/core/server/QueueServerModel.java
+++ QueueApp/QueueApp/src/main/java/org/example/core/server/QueueServerModel.java
@@ -1,7 +1,10 @@
 package org.example.core.server;
 
+import java.util.Map;
+
 public interface QueueServerModel {
 
     QueueServerModelBuilder builder();
 
+    <T extends ServerParameters<? extends Object>> Map<String, ServerParameters<T>> getParams();
 }
