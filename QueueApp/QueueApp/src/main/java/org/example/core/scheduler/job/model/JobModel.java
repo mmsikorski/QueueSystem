1278e698fe53d1dc9c1f51cb1421fc27b73877dc
mmsikorski <mm.sikorski3@gmail.com>
2023-01-21 14:32:05 +0100

    add job model

--- QueueApp/QueueApp/src/main/java/org/example/core/scheduler/job/model/JobModel.java
+++ QueueApp/QueueApp/src/main/java/org/example/core/scheduler/job/model/JobModel.java
@@ -1,7 +1,15 @@
 package org.example.core.scheduler.job.model;
 
+import org.example.core.scheduler.job.creator.JobLogic;
+
 public interface JobModel {
 
+    void execute();
+    void setExecutionTimeFrequency();
+    void setJobName(JobNames jobName);
+
+    JobNames getJobName();
+    default JobModel getJobModel() { return this; }
 
 
 }
